44febe33abff8561a9d93708b151748dbad1cf50
jhandschin <jhandschin86@gmail.com>
2021-02-17 10:30:30 -0700

    Update project3.py

--- project3.py
+++ project3.py
@@ -2,9 +2,39 @@
 # Imports
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
 import requests
+from bs4 import BeautifulSoup
+import re
 
-#%% 
 
+#%%
+html = requests.get(
+    'https://vgsales.fandom.com/wiki/List_of_highest-grossing_video_games')
+soup = BeautifulSoup(html.text, 'html5lib')
+soup.text
+
+
+# %%
+# find <tr><td><a class=extiw" GameTitle></td></tr>
+table = soup('table')
+
+
+#%%
+rows = []
+titles = []
+
+for row in soup.find_all('tr'):
+    rows.append(row)
+    for column in row.find_all('td'):
+        titles.append(column('a', {'class': 'extiw'}))
+        rows.append(column)
+    # for item in row.find_all
+
+#%%
+rows[1]
+
+
+# %%
+columns = ['title', 'Gross(w/o inflation), Release Year',
+           'Gross(as of year)', 'Gross(w/ inflation)', 'Genre']
